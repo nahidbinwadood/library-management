acdaa6d611f0187b01b2a479af1dd6a7538c781e
nahidbinwadood <nahidbinwadood@gmail.com>
2025-08-14 23:52:19 +0600

    fix: updated cors

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
-import config from './app/config';
 import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
 import router from './app/router/router';
 
 const app: Application = express();
 
 const corsOptions = {
-  origin: [config.client_base_url as string],
+  origin: ['http://localhost:3000'],
   credentials: true,
+  optionSuccessStatus: 200,
 };
 
 // parser:

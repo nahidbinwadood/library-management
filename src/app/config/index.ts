dd4ea6b147bc67f7c713e5042eff3ef6da11c641
nahidbinwadood <nahidbinwadood@gmail.com>
2025-08-14 23:29:07 +0600

    fix: update cors and env

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -5,7 +5,8 @@ dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
   port: process.env.PORT,
-  frontend_port: process.env.FRONTEND_PORT,
-  database_url: process.env.DATABASE_URL,
   client_base_url: process.env.CLIENT_BASE_URL,
+  live_client_base_url: process.env.LIVE_CLIENT_BASE_URL,
+  database_url: process.env.DATABASE_URL,
+  server_base_url: process.env.SERVER_BASE_URL,
 };

a1107271339eb876682e80de4926177d182f2017
nahidbinwadood <nahidbinwadood@gmail.com>
2025-08-15 16:30:18 +0600

    fix: updated cors

--- dist/app.js
+++ dist/app.js
@@ -9,20 +9,8 @@ const config_1 = __importDefault(require("./app/config"));
 const globalErrorHandler_1 = require("./app/middlewares/globalErrorHandler");
 const router_1 = __importDefault(require("./app/router/router"));
 const app = (0, express_1.default)();
-const allowedOrigins = [config_1.default.client_base_url, config_1.default.live_client_base_url];
-// const allowedOrigins = [
-//   'http://localhost:3000',
-//   'https://my-library-management-tau.vercel.app',
-// ];
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        }
-        else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: [config_1.default.client_base_url, config_1.default.live_client_base_url],
     credentials: true,
 };
 // parser:
--- src/app.ts
+++ src/app.ts
@@ -6,20 +6,8 @@ import router from './app/router/router';
 
 const app: Application = express();
 
-const allowedOrigins = [config.client_base_url, config.live_client_base_url];
-
-// const allowedOrigins = [
-//   'http://localhost:3000',
-//   'https://my-library-management-tau.vercel.app',
-// ];
 const corsOptions = {
-  origin: (origin: string | undefined, callback: any) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: [config.client_base_url!, config.live_client_base_url!],
   credentials: true,
 };
 

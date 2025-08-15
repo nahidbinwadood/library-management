4c42beece36d24d57f63b95de427c2ed3fa7cf01
nahidbinwadood <nahidbinwadood@gmail.com>
2025-08-15 16:32:26 +0600

    fix: updated cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
-import config from './app/config';
 import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
 import router from './app/router/router';
 
 const app: Application = express();
 
+// const allowedOrigins = [config.client_base_url, config.live_client_base_url];
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://my-library-management-tau.vercel.app',
+];
 const corsOptions = {
-  origin: [config.client_base_url!, config.live_client_base_url!],
+  origin: (origin: string | undefined, callback: any) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
 };
 

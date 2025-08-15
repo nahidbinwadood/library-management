210425ae7342836614e8abd2af294d4c4054285e
nahidbinwadood <nahidbinwadood@gmail.com>
2025-08-15 16:20:23 +0600

    fix: updated global error

--- dist/app/middlewares/globalErrorHandler.js
+++ dist/app/middlewares/globalErrorHandler.js
@@ -4,9 +4,6 @@ exports.globalErrorHandler = void 0;
 const handleMongooseError_1 = require("../errors/handleMongooseError");
 const globalErrorHandler = (error, req, res, next) => {
     const { statusCode, message, error: formattedError, } = (0, handleMongooseError_1.handleMongooseError)(error);
-    // Ensure CORS headers are always set
-    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
     res.status(statusCode).json({
         success: false,
         message,

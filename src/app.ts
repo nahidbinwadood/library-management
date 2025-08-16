c007c84019fa629cb1d1aba570d04a296a08b2e1
nahidbinwadood <nahidbinwadood@gmail.com>
2025-08-16 10:24:36 +0600

    fix: updated env file configuration

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import config from './app/config';
 import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
 import router from './app/router/router';
 
 const app: Application = express();
 
-// const allowedOrigins = [config.client_base_url, config.live_client_base_url];
+const allowedOrigins = [config.client_local_url, config.client_live_url];
 
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://my-library-management-tau.vercel.app',
-];
+// const allowedOrigins = [
+//   'http://localhost:3000',
+//   'https://my-library-management-tau.vercel.app',
+// ];
 const corsOptions = {
   origin: (origin: string | undefined, callback: any) => {
     if (!origin || allowedOrigins.includes(origin)) {
